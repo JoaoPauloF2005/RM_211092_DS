bc900aee10e0f2d447dc75d4f8967cc5cf7f098f
Usuario <Usuario@CPD03-M04>
2022-10-19 11:06:44 -0300

    Atualizando Banco de Dados

--- RM_211092/Models/Cidade.cs
+++ RM_211092/Models/Cidade.cs
@@ -14,6 +14,7 @@ namespace RM_211092
         public string uf { get; set; }
 
 
+
         public void Incluir()
         {
             try
@@ -22,6 +23,7 @@ namespace RM_211092
                 Banco.Comando = new MySqlCommand("INSERT INTO Cidades (nome, uf) VALUES (@nome, @uf)", Banco.Conexao);
                 Banco.Comando.Parameters.AddWithValue("@nome", nome);
                 Banco.Comando.Parameters.AddWithValue("@uf", uf);
+                Banco.Comando.Parameters.AddWithValue("@id", id);
                 Banco.Comando.ExecuteNonQuery();
                 Banco.FecharConexao();
             }
@@ -30,5 +32,61 @@ namespace RM_211092
                 MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
+        public void Alterar()
+        {
+            try
+            {
+                Banco.AbrirConexao();
+                Banco.Comando = new MySqlCommand("Update cidades set nome = @nome, uf = @uf where id = @id", Banco.Conexao);
+                Banco.Comando.Parameters.AddWithValue("@nome", nome);
+                Banco.Comando.Parameters.AddWithValue("@uf", uf);
+                Banco.Comando.Parameters.AddWithValue("@id", id);
+                Banco.Comando.ExecuteNonQuery();
+                Banco.FecharConexao();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        public void Excluir()
+        {
+            try
+            {
+                Banco.AbrirConexao();
+                Banco.Comando = new MySqlCommand("delete from cidades where id = @id", Banco.Conexao);
+                Banco.Comando.Parameters.AddWithValue("@id", id);
+                Banco.Comando.ExecuteNonQuery();
+                Banco.FecharConexao();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public DataTable Consultar()
+        {
+            try
+            {
+                Banco.Comando = new MySqlCommand("SELECT * FROM Cidades where nome Like @Nome order by nome", Banco.Conexao);
+                Banco.Comando.Parameters.AddWithValue("@Nome", nome + "%");
+                Banco.Adaptador = new MySqlDataAdapter(Banco.Comando);
+                Banco.datTabela = new DataTable();
+                Banco.Adaptador.Fill(Banco.datTabela);
+                Banco.FecharConexao();
+                return Banco.datTabela;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+    
     }
+
 }
